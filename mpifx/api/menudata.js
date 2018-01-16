d0b82d3134c16895be16d99546cf1e1ccacd1674
Bálint Aradi <aradi@uni-bremen.de>
2018-01-16 16:16:40 +0100

    Add description of mpifx and scalapackfx

---
+++ mpifx/api/menudata.js
@@ -0,0 +1,23 @@
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Modules",url:"namespaces.html",children:[
+{text:"Modules List",url:"namespaces.html"},
+{text:"Module Members",url:"namespacemembers.html",children:[
+{text:"All",url:"namespacemembers.html",children:[
+{text:"m",url:"namespacemembers.html#index_m"}]},
+{text:"Functions/Subroutines",url:"namespacemembers_func.html",children:[
+{text:"m",url:"namespacemembers_func.html#index_m"}]}]}]},
+{text:"Data Types List",url:"annotated.html",children:[
+{text:"Data Types List",url:"annotated.html"},
+{text:"Data Types",url:"classes.html"},
+{text:"Data Fields",url:"functions.html",children:[
+{text:"All",url:"functions.html",children:[
+{text:"i",url:"functions.html#index_i"},
+{text:"m",url:"functions_m.html#index_m"},
+{text:"r",url:"functions_r.html#index_r"},
+{text:"s",url:"functions_s.html#index_s"}]},
+{text:"Functions/Subroutines",url:"functions_func.html",children:[
+{text:"i",url:"functions_func.html#index_i"},
+{text:"m",url:"functions_func_m.html#index_m"},
+{text:"s",url:"functions_func_s.html#index_s"}]},
+{text:"Variables",url:"functions_vars.html"}]}]}]}
---
+++ scalapackfx/manual/_sources/routines.txt
@@ -0,0 +1,8 @@
+.. _sec_routines:
+
+List of routines
+================
+
+You can generate the list and the description of the SCALAPACKFX routines via
+doxygen (see folder `doc/doxygen/` in the source tree) or examples as sphix
+documentation.
