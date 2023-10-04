230aa4ef89f347836f9dcd9e5c2e1d5a2b12ab97
Bálint Aradi <aradi@uni-bremen.de>
2023-10-04 15:23:26 +0200

    Rebuild to obtain more recent JQuery version

--- mpifx/api/menudata.js
+++ mpifx/api/menudata.js
@@ -1,3 +1,27 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
 {text:"Modules",url:"namespaces.html",children:[
@@ -7,17 +31,26 @@ var menudata={children:[
 {text:"m",url:"namespacemembers.html#index_m"}]},
 {text:"Functions/Subroutines",url:"namespacemembers_func.html",children:[
 {text:"m",url:"namespacemembers_func.html#index_m"}]}]}]},
-{text:"Data Types List",url:"annotated.html",children:[
+{text:"Data Types",url:"annotated.html",children:[
 {text:"Data Types List",url:"annotated.html"},
-{text:"Data Types",url:"classes.html"},
+{text:"Data Type Index",url:"classes.html"},
 {text:"Data Fields",url:"functions.html",children:[
 {text:"All",url:"functions.html",children:[
-{text:"i",url:"functions.html#index_i"},
+{text:"a",url:"functions.html#index_a"},
+{text:"c",url:"functions_c.html#index_c"},
+{text:"f",url:"functions_f.html#index_f"},
+{text:"i",url:"functions_i.html#index_i"},
+{text:"l",url:"functions_l.html#index_l"},
 {text:"m",url:"functions_m.html#index_m"},
 {text:"r",url:"functions_r.html#index_r"},
-{text:"s",url:"functions_s.html#index_s"}]},
+{text:"s",url:"functions_s.html#index_s"},
+{text:"u",url:"functions_u.html#index_u"}]},
 {text:"Functions/Subroutines",url:"functions_func.html",children:[
-{text:"i",url:"functions_func.html#index_i"},
+{text:"a",url:"functions_func.html#index_a"},
+{text:"f",url:"functions_func_f.html#index_f"},
+{text:"i",url:"functions_func_i.html#index_i"},
+{text:"l",url:"functions_func_l.html#index_l"},
 {text:"m",url:"functions_func_m.html#index_m"},
-{text:"s",url:"functions_func_s.html#index_s"}]},
+{text:"s",url:"functions_func_s.html#index_s"},
+{text:"u",url:"functions_func_u.html#index_u"}]},
 {text:"Variables",url:"functions_vars.html"}]}]}]}
